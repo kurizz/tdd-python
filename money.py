05fa836695a061af5ced6b1a41085ab266fa8b7d
kurizz <umozon63313@gmail.com>
2026-02-12 13:57:06 +0900

    「testFrancMultiplication 削除する？」を TODO に追記
    times の移動のやり方がすぐに思いつかなかったので、とりあえず利用者側コードからサブクラスの参照を引き剥がした。利用者側から使わずにテストが通るならば、サブクラスを削除できることになるから。
    まずは Factory を使って、Dollar/Franc を返すようにし、times はシグニチャをあわせるにとどめた。
    サブクラスを隠蔽することで、test_franc_multi が冗長なことが露見してきたが今回はそのままで先に進んだ。

--- money.py
+++ money.py
@@ -1,7 +1,29 @@
-class Money():
+from typing import TYPE_CHECKING
+from abc import ABC, abstractmethod
+
+if TYPE_CHECKING:
+    from dollar import Dollar
+    from franc import Franc
+
+
+class Money(ABC):
 
     def __init__(self, amount: int):
         self._amount = amount
 
+    @abstractmethod
+    def times(self, multiplier: int):
+        pass
+
     def __eq__(self, money: 'Money') -> bool:
         return self._amount == money._amount and type(self) == type(money)
+
+    @staticmethod
+    def dollar(amount: int) -> 'Dollar':
+        from dollar import Dollar
+        return Dollar(amount)
+
+    @staticmethod
+    def franc(amount: int) -> 'Franc':
+        from franc import Franc
+        return Franc(amount)
--- tests/test_money.py
+++ tests/test_money.py
@@ -1,23 +1,22 @@
-from dollar import Dollar
-from franc import Franc
+from money import Money
 
 class TestMoney():
 
-    def test_multiplication(self):
-        five = Dollar(5)
-        assert Dollar(10) == five.times(2)
-        assert Dollar(15) == five.times(3)
+    def test_dollar_multiplication(self):
+        five = Money.dollar(5)
+        assert Money.dollar(10) == five.times(2)
+        assert Money.dollar(15) == five.times(3)
 
     def test_equality(self):
-        assert Dollar(5) == Dollar(5)
-        assert not Dollar(5) == Dollar(6)
+        assert Money.dollar(5) == Money.dollar(5)
+        assert not Money.dollar(5) == Money.dollar(6)
 
-        assert Franc(5) == Franc(5)
-        assert not Franc(5) == Franc(6)
+        assert Money.franc(5) == Money.franc(5)
+        assert not Money.franc(5) == Money.franc(6)
 
-        assert not Dollar(5) == Franc(5)
+        assert not Money.dollar(5) == Money.franc(5)
 
     def test_franc_multiplication(self):
-        five = Franc(5)
-        assert Franc(10) == five.times(2)
-        assert Franc(15) == five.times(3)
+        five = Money.franc(5)
+        assert Money.franc(10) == five.times(2)
+        assert Money.franc(15) == five.times(3)
