c850849689c762e2ad3c7ce1d49c53f59674ec07
kurizz <umozon63313@gmail.com>
2026-02-14 00:49:22 +0900

    $5+$5=10$ (未完)
    $5+10CHF は歩幅が大きすぎるので $5+$5=10$ を作ることにした。
    Imposter を持ち込んだ。このタイミングでこのアイデアを持ち込める再現性はない。ただ、そのための安心して作業できる環境作りが TDD ともいえる。
    為替計算をするための設計にメタファーを持ち込んだ。ひらめき次第でもあるが設計の助けとなる。
    メタファーの導入とそれに伴ってコンパイルが通るところまでとりあえず書き直した。
    本当にやりたいことをするための準備 (グリーンバー) を終えたので安心してリファクタリングに臨める。
    amount のリファクタリングもついでに行った。

--- money.py
+++ money.py
@@ -1,24 +1,31 @@
-from abc import ABC
+from expression import Expression
 
 
-class Money(ABC):
+class Money(Expression):
 
     def __init__(self, amount: int, currency: str):
         self._amount = amount
         self._currency = currency
 
     def times(self, multiplier: int) -> 'Money':
-        return Money(self._amount * multiplier, self.currency)
+        return Money(self.amount * multiplier, self.currency)
+
+    def plus(self, addend: int) -> Expression:
+        return Money(self.amount + addend.amount, self.currency)
 
     @property
     def currency(self) -> str:
         return self._currency
 
+    @property
+    def amount(self) -> int:
+        return self._amount
+
     def __eq__(self, money: 'Money') -> bool:
-        return self._amount == money._amount and self.currency == money.currency
+        return self.amount == money.amount and self.currency == money.currency
 
     def __repr__(self):
-        return str(self._amount) + self.currency
+        return str(self.amount) + self.currency
 
     @staticmethod
     def dollar(amount: int) -> 'Money':
--- tests/test_money.py
+++ tests/test_money.py
@@ -1,4 +1,5 @@
 from money import Money
+from bank import Bank
 
 class TestMoney():
 
@@ -6,6 +7,11 @@ class TestMoney():
         five = Money.dollar(5)
         assert Money.dollar(10) == five.times(2)
         assert Money.dollar(15) == five.times(3)
+
+    def test_simple_addtion(self):
+        sum = Money.dollar(5).plus(Money.dollar(5))
+        reduced = Bank().reduce(sum, "USD")
+        assert Money.dollar(10) == reduced
 
     def test_equality(self):
         assert Money.dollar(5) == Money.dollar(5)
@@ -15,3 +21,6 @@ class TestMoney():
     def test_currenry(self):
         assert "USD" == Money.dollar(1).currency
         assert "CHF" == Money.franc(1).currency
+
+    def test_amount(self):
+        assert 10 is Money.dollar(10).amount
