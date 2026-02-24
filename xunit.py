d9fbc02c98681fae96a9822ba48a4fea3fdd6826
kurizz <umozon63313@gmail.com>
2026-02-24 09:17:13 +0900

    呼び出しをインターフェース経由にすることで、２つの部分を分離した。

--- xunit.py
+++ xunit.py
@@ -3,13 +3,16 @@ class WasRun:
     def __init__(self, name):
         self.wasRun = None
 
+    def run(self):
+        self.testMethod()
+
     def testMethod(self):
         self.wasRun = 1
 
 
 test = WasRun("testMeshod")
 print(test.wasRun)
-test.testMethod()
+test.run()
 print(test.wasRun)
 
 print("---8<---8<---8<---")
