708f8127154ff4ace75b9c9ad4feca64bcab4951
kurizz <umozon63313@gmail.com>
2026-02-25 08:41:58 +0900

    setUp を移動させてシンプルにした。(19 章終わり)
    pluggable selector は便利だが相変わらず読みづらい。シンプルなときぐらいしか使い道ないかもしれない。
    テストをシンプルにした。グリーンな他テストがあるので自信を持って重複を削除した。
    さらにシンプルにするためにフィクスチャを使って重複を排除した。

--- xunit.py
+++ xunit.py
@@ -14,11 +14,8 @@ class TestCase:
 
 class WasRun(TestCase):
 
-    def __init__(self, name):
-        self.wasRun = None
-        super().__init__(name)
-
     def setUp(self):
+        self.wasRun = None
         self.wasSetUp = 1
 
     def testMethod(self):
@@ -27,16 +24,16 @@ class WasRun(TestCase):
 
 class TestCaseTest(TestCase):
 
+    def setUp(self):
+        self.test = WasRun("testMethod")
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert not test.wasRun
-        test.run()
-        assert test.wasRun
+        self.test.run()
+        assert self.test.wasRun
 
     def testSetup(self):
-        test = WasRun("testMethod")
-        test.run()
-        assert test.wasSetUp
+        self.test.run()
+        assert self.test.wasSetUp
 
 
 TestCaseTest("testRunning").run()
