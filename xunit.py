73fd92139931a7d6a2af820dc94266cd1de1bcef
kurizz <umozon63313@gmail.com>
2026-02-25 08:04:42 +0900

    第 19 章: 前準備
    setUp の作成。Green までに 2 step 必要だった。非常に壊れやすい環境なので本来なら 1 step ずつテストを動かして様子見を見るといいだろう。

--- xunit.py
+++ xunit.py
@@ -3,7 +3,11 @@ class TestCase:
     def __init__(self, name):
         self.name = name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
@@ -13,6 +17,9 @@ class WasRun(TestCase):
     def __init__(self, name):
         self.wasRun = None
         super().__init__(name)
+
+    def setUp(self):
+        self.wasSetUp = 1
 
     def testMethod(self):
         self.wasRun = 1
@@ -26,5 +33,11 @@ class TestCaseTest(TestCase):
         test.run()
         assert test.wasRun
 
+    def testSetup(self):
+        test = WasRun("testMethod")
+        test.run()
+        assert test.wasSetUp
+
 
 TestCaseTest("testRunning").run()
+TestCaseTest("testSetup").run()
