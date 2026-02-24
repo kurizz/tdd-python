f2ac8a73462a253afdc6e9af21f712440d0d7efa
kurizz <umozon63313@gmail.com>
2026-02-24 09:31:02 +0900

    テストメソッドを動的に呼び出すように修正。
    オブジェクトがあればそれを返してもらい、受け取った側で呼び出す ... ということができる。
    今までベタ書きといえば値だったが今回は、コード、つまり呼び出し処理そのものだったが原則は同じ。
    この時点で WasRun が起動記録とメソッド動的呼び出しの 2 つの仕事を担うようになった。早いうちに分割するのがいいだろう。

--- xunit.py
+++ xunit.py
@@ -2,15 +2,17 @@ class WasRun:
 
     def __init__(self, name):
         self.wasRun = None
+        self.name = name
 
     def run(self):
-        self.testMethod()
+        method = getattr(self, self.name)
+        method()
 
     def testMethod(self):
         self.wasRun = 1
 
 
-test = WasRun("testMeshod")
+test = WasRun("testMethod")
 print(test.wasRun)
 test.run()
 print(test.wasRun)
