f7956534b19b42e5ef120eb51a9bbe12130456ba
kurizz <umozon63313@gmail.com>
2026-02-25 13:11:40 +0900

    テスト内容を testSetUp にまとめた。
    全体のログを見るようにしたことで、テスト内容に重複が発生した。片方を削除。

--- xunit.py
+++ xunit.py
@@ -15,11 +15,9 @@ class TestCase:
 class WasRun(TestCase):
 
     def setUp(self):
-        self.wasRun = None
         self.log = "setUp "
 
     def testMethod(self):
-        self.wasRun = 1
         self.log = self.log + "testMethod "
 
 
@@ -28,14 +26,9 @@ class TestCaseTest(TestCase):
     def setUp(self):
         self.test = WasRun("testMethod")
 
-    def testRunning(self):
-        self.test.run()
-        assert self.test.wasRun
-
-    def testSetup(self):
+    def testTemplateMethod(self):
         self.test.run()
         assert "setUp testMethod " == self.test.log
 
 
-TestCaseTest("testRunning").run()
-TestCaseTest("testSetup").run()
+TestCaseTest("testTemplateMethod").run()
