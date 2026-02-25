6f514babe254d8b077716c1d1a32bb02592d29c6
kurizz <umozon63313@gmail.com>
2026-02-25 09:49:56 +0900

    第 20 章: 後片付け
    フラグではなく log を取るようにした。
    フラグだとテストのタイミングを制御するのが面倒だし、肝心の setup/teardown の動作順がテストから保証できないので log にしてみた。

--- xunit.py
+++ xunit.py
@@ -16,10 +16,11 @@ class WasRun(TestCase):
 
     def setUp(self):
         self.wasRun = None
-        self.wasSetUp = 1
+        self.log = "setUp "
 
     def testMethod(self):
         self.wasRun = 1
+        self.log = self.log + "testMethod "
 
 
 class TestCaseTest(TestCase):
@@ -33,7 +34,7 @@ class TestCaseTest(TestCase):
 
     def testSetup(self):
         self.test.run()
-        assert self.test.wasSetUp
+        assert "setUp testMethod " == self.test.log
 
 
 TestCaseTest("testRunning").run()
